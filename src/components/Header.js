eaf1fa726c835b71054e9dd46dba5c9b7f143486
ruslanelishaev <ruslan@byondxr.com>
2022-08-01 09:54:07 +0300

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,21 @@ import React from 'react'
 import NavBar from './NavBar'
 import Logo from '../assets/logo.png'
 import { useLocation } from '@reach/router'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Header = (props) => {
 	const location = useLocation()
 	return (
 		<header className={`header ${location.pathname === '/' ? 'home' : 'inner-pages'}`}>
-			<img className={'logo'} src={Logo} alt='' />
+			{/*<img className={'logo'} src={Logo} alt='' />*/}
+			<StaticImage
+				className={'logo'}
+				src='../assets/logo.png'
+				layout='constrained'
+				alt='איתן כבריאן'
+				placeholder='none'
+				quality={90}
+			/>
 			<NavBar pathname={location.pathname} />
 		</header>
 	)
